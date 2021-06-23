7524505a34ff4cc43fdd178c391efdcac15b1c65
healtsoft <72839848+healtsoft@users.noreply.github.com>
2021-06-23 12:41:13 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/historia_clinica/create', 'HistoriaClinicaController@store');
-
-Route::get('/user', 'PacienteController@allUsers');
-
-Route::resource('/pxs', [PacienteController::class]);
